79b7957ad0b9b47d08c866b4264c550108af5f3f
Pham Xuan Bach <53576898+bachpham995@users.noreply.github.com>
2021-07-29 23:21:50 +0700

    [BachPham]Config front end

--- frontendExt/src/index.js
+++ frontendExt/src/index.js
@@ -11,6 +11,7 @@ import reducers from './reducers';
 import { Provider } from 'react-redux';
 import { applyMiddleware, createStore } from 'redux';
 import thunk from 'redux-thunk';
+import 'bootstrap/dist/css/bootstrap.css';
 
 const store = createStore(reducers, applyMiddleware(thunk));
 
