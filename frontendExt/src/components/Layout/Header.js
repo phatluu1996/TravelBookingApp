bc67688d6bf45f09f5b0fd0e4d3b5228879223a4
Pham Xuan Bach <53576898+bachpham995@users.noreply.github.com>
2021-07-29 23:23:13 +0700

    Revert "[BachPham]Config front end"
    This reverts commit 79b7957ad0b9b47d08c866b4264c550108af5f3f.

--- frontendExt/src/components/Layout/Header.js
+++ frontendExt/src/components/Layout/Header.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import HeaderViewed from './Header/HeaderViewed';
 
 const Header = () => {
     return (<>
@@ -51,7 +50,54 @@ const Header = () => {
                         <a href="#" className="social-pinterest"></a>
                         <a href="#" className="social-instagram"></a>
                     </div>
-                    <HeaderViewed/>
+                    <div className="header-viewed">
+                        <a href="#" className="header-viewed-btn">recently viewed</a>
+
+                        <div className="viewed-drop">
+                            <div className="viewed-drop-a">
+
+                                <div className="viewed-item">
+                                    <div className="viewed-item-l">
+                                        <a href="#"><img alt="" src="img/v-item-01.jpg" /></a>
+                                    </div>
+                                    <div className="viewed-item-r">
+                                        <div className="viewed-item-lbl"><a href="#">Andrassy Thai Hotel</a></div>
+                                        <div className="viewed-item-cat">location: thailand</div>
+                                        <div className="viewed-price">152$</div>
+                                    </div>
+                                    <div className="clear"></div>
+                                </div>
+
+
+                                <div className="viewed-item">
+                                    <div className="viewed-item-l">
+                                        <a href="#"><img alt="" src="img/v-item-02.jpg" /></a>
+                                    </div>
+                                    <div className="viewed-item-r">
+                                        <div className="viewed-item-lbl"><a href="#">Ermin's Hotel</a></div>
+                                        <div className="viewed-item-cat">location: dubai</div>
+                                        <div className="viewed-price">300$</div>
+                                    </div>
+                                    <div className="clear"></div>
+                                </div>
+
+
+                                <div className="viewed-item">
+                                    <div className="viewed-item-l">
+                                        <a href="#"><img alt="" src="img/v-item-03.jpg" /></a>
+                                    </div>
+                                    <div className="viewed-item-r">
+                                        <div className="viewed-item-lbl"><a href="#">Best Western Hotel Reither</a></div>
+                                        <div className="viewed-item-cat">location: berlin</div>
+                                        <div className="viewed-price">2300$</div>
+                                    </div>
+                                    <div className="clear"></div>
+                                </div>
+
+                            </div>
+                        </div>
+
+                    </div>
                     <div className="header-lang">
                         <a href="#"><img alt="" src="img/en.gif" /></a>
                         <div className="langs-drop">
