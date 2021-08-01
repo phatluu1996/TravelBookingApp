555d338d82217f079f774a3467feded071870337
Minh Ngoc <minhngoclenguyen@gmail.com>
2021-08-01 19:53:15 +0700

    [Ngoc] -backend

--- backend/src/main/java/com/travelbooking/backend/repository/AirlineRepository.java
+++ backend/src/main/java/com/travelbooking/backend/repository/AirlineRepository.java
@@ -2,6 +2,8 @@ package com.travelbooking.backend.repository;
 
 import com.travelbooking.backend.models.Airline;
 import com.travelbooking.backend.models.Flight;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
@@ -15,6 +17,6 @@ public interface AirlineRepository extends JpaRepository<Airline, Long>, JpaSpec
     Airline getByAccountId(Long id);
 
     @Query("SELECT a.flights from Airline as a where a.id = :airlineId")
-    Collection<Flight> listFlightByAirline(@Param("airlineId") Long airlineId);
+    Page<Flight> listFlightByAirline(@Param("airlineId") Long airlineId, Pageable pageable);
 
 }
